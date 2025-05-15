b3de0cc24875ed01dcf2316e8d960ec267eaeb5d
Lohani Pratik <pratik.lohani@student.howest.be>
2025-05-15 21:37:38 +0200

    use mockRoom to fix tests

--- tests/UnitTests/Application/BookMovieEventHandlerTests.cs
+++ tests/UnitTests/Application/BookMovieEventHandlerTests.cs
@@ -5,6 +5,7 @@ using Howestprime.Movies.Application.Contracts.Ports;
 using Howestprime.Movies.Domain.Entities;
 using Howestprime.Movies.Domain.Events;
 using Xunit;
+using System.Collections.Generic;
 
 namespace UnitTests.Application
 {
@@ -22,17 +23,30 @@ namespace UnitTests.Application
             public Task<MovieEvent> GetByIdWithBookingsAsync(Guid movieEventId) => Task.FromResult(MovieEvent!);
             public Task UpdateAsync(MovieEvent movieEvent) { Updated = true; return Task.CompletedTask; }
         }
+        
         private class FakeUnitOfWork : IUnitOfWork { public Task CommitAsync() => Task.CompletedTask; }
+        
         private class FakeEventPublisher : IEventPublisher { public BookingOpened? Published; public Task PublishAsync(BookingOpened e) { Published = e; return Task.CompletedTask; } }
+        
+        private class FakeRoomRepository : IRoomRepository 
+        {
+            public Room? Room { get; set; } = new Room { Id = Guid.NewGuid(), Name = "Test Room", Capacity = 100 };
+            
+            public Task<Room?> GetByIdAsync(Guid id) => Task.FromResult(Room);
+            public Task SeedRoomsAsync() => Task.CompletedTask;
+        }
 
         [Fact]
         public async Task HandleAsync_ValidCommand_BooksEventAndPublishes()
         {
-            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new System.Collections.Generic.List<Booking>(), Visitors = 0, Capacity = 10 };
+            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new List<Booking>(), Visitors = 0, Capacity = 10 };
             var repo = new FakeMovieEventRepository { MovieEvent = movieEvent };
             var uow = new FakeUnitOfWork();
             var publisher = new FakeEventPublisher();
-            var handler = new BookMovieEventHandler(repo, uow, publisher);
+            var roomRepo = new FakeRoomRepository();
+            
+            var handler = new BookMovieEventHandler(repo, uow, publisher, roomRepo);
+            
             var command = new BookMovieEventCommand { MovieEventId = movieEvent.Id, StandardVisitors = 1, DiscountVisitors = 1, RoomName = "Room 1" };
             var result = await handler.HandleAsync(command);
             Assert.NotNull(result);
@@ -45,11 +59,14 @@ namespace UnitTests.Application
         [Fact]
         public async Task HandleAsync_InvalidVisitors_Throws()
         {
-            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new System.Collections.Generic.List<Booking>(), Visitors = 0, Capacity = 10 };
+            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new List<Booking>(), Visitors = 0, Capacity = 10 };
             var repo = new FakeMovieEventRepository { MovieEvent = movieEvent };
             var uow = new FakeUnitOfWork();
             var publisher = new FakeEventPublisher();
-            var handler = new BookMovieEventHandler(repo, uow, publisher);
+            var roomRepo = new FakeRoomRepository();
+            
+            var handler = new BookMovieEventHandler(repo, uow, publisher, roomRepo);
+            
             var command = new BookMovieEventCommand { MovieEventId = movieEvent.Id, StandardVisitors = -1, DiscountVisitors = 0, RoomName = "Room 1" };
             await Assert.ThrowsAsync<ArgumentException>(() => handler.HandleAsync(command));
         }
@@ -57,11 +74,14 @@ namespace UnitTests.Application
         [Fact]
         public async Task HandleAsync_OverCapacity_Throws()
         {
-            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new System.Collections.Generic.List<Booking>(), Visitors = 9, Capacity = 10 };
+            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new List<Booking>(), Visitors = 9, Capacity = 10 };
             var repo = new FakeMovieEventRepository { MovieEvent = movieEvent };
             var uow = new FakeUnitOfWork();
             var publisher = new FakeEventPublisher();
-            var handler = new BookMovieEventHandler(repo, uow, publisher);
+            var roomRepo = new FakeRoomRepository();
+            
+            var handler = new BookMovieEventHandler(repo, uow, publisher, roomRepo);
+            
             var command = new BookMovieEventCommand { MovieEventId = movieEvent.Id, StandardVisitors = 2, DiscountVisitors = 0, RoomName = "Room 1" };
             await Assert.ThrowsAsync<InvalidOperationException>(() => handler.HandleAsync(command));
         }
@@ -69,11 +89,14 @@ namespace UnitTests.Application
         [Fact]
         public async Task HandleAsync_BookingTooFarInAdvance_Throws()
         {
-            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new System.Collections.Generic.List<Booking>(), Visitors = 0, Capacity = 10, Date = DateTime.UtcNow.AddDays(15) };
+            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new List<Booking>(), Visitors = 0, Capacity = 10, Date = DateTime.UtcNow.AddDays(15) };
             var repo = new FakeMovieEventRepository { MovieEvent = movieEvent };
             var uow = new FakeUnitOfWork();
             var publisher = new FakeEventPublisher();
-            var handler = new BookMovieEventHandler(repo, uow, publisher);
+            var roomRepo = new FakeRoomRepository();
+            
+            var handler = new BookMovieEventHandler(repo, uow, publisher, roomRepo);
+            
             var command = new BookMovieEventCommand { MovieEventId = movieEvent.Id, StandardVisitors = 1, DiscountVisitors = 0, RoomName = "Room 1" };
             await Assert.ThrowsAsync<InvalidOperationException>(() => handler.HandleAsync(command));
         }
@@ -81,11 +104,14 @@ namespace UnitTests.Application
         [Fact]
         public async Task HandleAsync_ZeroVisitors_Throws()
         {
-            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new System.Collections.Generic.List<Booking>(), Visitors = 0, Capacity = 10 };
+            var movieEvent = new MovieEvent { Id = Guid.NewGuid(), Bookings = new List<Booking>(), Visitors = 0, Capacity = 10 };
             var repo = new FakeMovieEventRepository { MovieEvent = movieEvent };
             var uow = new FakeUnitOfWork();
             var publisher = new FakeEventPublisher();
-            var handler = new BookMovieEventHandler(repo, uow, publisher);
+            var roomRepo = new FakeRoomRepository();
+            
+            var handler = new BookMovieEventHandler(repo, uow, publisher, roomRepo);
+            
             var command = new BookMovieEventCommand { MovieEventId = movieEvent.Id, StandardVisitors = 0, DiscountVisitors = 0, RoomName = "Room 1" };
             await Assert.ThrowsAsync<ArgumentException>(() => handler.HandleAsync(command));
         }
@@ -96,7 +122,10 @@ namespace UnitTests.Application
             var repo = new FakeMovieEventRepository { MovieEvent = null };
             var uow = new FakeUnitOfWork();
             var publisher = new FakeEventPublisher();
-            var handler = new BookMovieEventHandler(repo, uow, publisher);
+            var roomRepo = new FakeRoomRepository();
+            
+            var handler = new BookMovieEventHandler(repo, uow, publisher, roomRepo);
+            
             var command = new BookMovieEventCommand
             {
                 MovieEventId = Guid.NewGuid(),
