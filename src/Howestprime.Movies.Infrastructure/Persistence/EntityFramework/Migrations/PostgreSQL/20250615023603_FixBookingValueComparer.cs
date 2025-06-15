2689d89004708b56426b15df93eee517f1d5f90a
Lohani Pratik <pratik.lohani@student.howest.be>
2025-06-15 04:36:44 +0200

    Migrations Seats

---
+++ src/Howestprime.Movies.Infrastructure/Persistence/EntityFramework/Migrations/PostgreSQL/20250615023603_FixBookingValueComparer.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Howestprime.Movies.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBookingValueComparer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_MovieEvents_MovieEventId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Visitors",
+                table: "MovieEvents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MovieEventId",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_MovieEvents_MovieEventId",
+                table: "Bookings",
+                column: "MovieEventId",
+                principalTable: "MovieEvents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_MovieEvents_MovieEventId",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Visitors",
+                table: "MovieEvents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MovieEventId",
+                table: "Bookings",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_MovieEvents_MovieEventId",
+                table: "Bookings",
+                column: "MovieEventId",
+                principalTable: "MovieEvents",
+                principalColumn: "Id");
+        }
+    }
+}
